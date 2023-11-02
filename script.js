1c303376002eaff0176eaf7eb74306edf70c68f7
aachal01 <aachalmodak10@gmail.com>
2023-11-02 12:00:23 +0530

    added slider progress

--- script.js
+++ script.js
@@ -76,3 +76,10 @@ copyButton.addEventListener('click', function(){
     navigator.clipboard.writeText(finalPass);
     alert("Copied the text: " + finalPass);
 })
+
+for (let e of document.querySelectorAll('input[type="range"].slider-progress')) {
+    e.style.setProperty('--value', e.value);
+    e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+    e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+    e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+  }
